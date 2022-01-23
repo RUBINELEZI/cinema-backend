16b9cbabe750b0becd50070c586fda5fe5d1ffc2
Rubin <rubinelezi0@gmail.com>
2022-01-23 19:53:01 +0100

    successful cruds

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,72 +1,83 @@
-
 const pool = require("../db");
+var format = require('pg-format');
 
-
-  const allMovies = async (req, res) => {  
+const allMovies = async (req, res) => {
     try {
         const result = await pool.query(
-          "SELECT * FROM film"
+            "SELECT * FROM film"
         );
-    
 
-        if (!result.rows[0]) return res.json({
+
+        if (!result.rows) return res.json({
             error: {
-              status: 400,
+                status: 400,
             }
-          });
+        });
 
-        res.json(result.rows[0]);
-      } catch (err) {
+        res.json(result.rows);
+    } catch (err) {
         console.error(err.message);
-      }
+    }
 }
 
-const movideById = async (req, res) => {  
+const movideById = async (req, res) => {
     try {
         const result = await pool.query(
-          `SELECT * FROM film WHERE film_id = ${req.params.id}`
+            `SELECT *
+             FROM film
+             WHERE film_id = ${req.params.id}`
         );
-        
+
         res.json(result.rows[0]);
-      } catch (err) {
+    } catch (err) {
         console.error(err.message);
-      }
+    }
 }
 
-const addMovie = async (req, res) => {  
+const addMovie = async (req, res) => {
+    const body = req.body
     try {
-        const newTodo = await pool.query(
-          "SELECT * FROM film"
-        );
-    
-        res.json(newTodo.rows[0]);
-      } catch (err) {
+        var values = [
+            [body.film_id, body.titulli, body.zhaner, body.dimensioni, body.kohezgjatja, body.mosha_lejuar]
+        ]
+
+        const result = await pool.query(format(
+            `INSERT INTO FILM (film_id, titulli, zhaner, dimensioni, kohezgjatja, mosha_lejuar)
+             VALUES %L`,
+            values
+        ));
+
+        res.json(result.rows);
+    } catch (err) {
         console.error(err.message);
-      }
+    }
 }
 
-const updateMovie = async (req, res) => {  
+const updateMovie = async (req, res) => {
+    const body = req.body
+
     try {
-        const newTodo = await pool.query(
-          "SELECT * FROM film"
+        const id = parseInt(req.params.id);
+        const result = await pool.query(
+            `UPDATE film set titulli = $1, zhaner = $2, dimensioni = $3, kohezgjatja = $4, mosha_lejuar = $5
+             WHERE film_id = $6`,
+            [body.titulli, body.zhaner, body.dimensioni, body.kohezgjatja, body.mosha_lejuar, id]
         );
-    
-        res.json(newTodo.rows[0]);
-      } catch (err) {
+
+        res.json('Succes');
+    } catch (err) {
         console.error(err.message);
-      }
+    }
 }
 
-const deleteMovie = async (req, res) => {  
+const deleteMovie = async (req, res) => {
+    const id = parseInt(req.params.id);
     try {
-        const newTodo = await pool.query(
-          "SELECT * FROM film"
-        );
-    
-        res.json(newTodo.rows[0]);
-      } catch (err) {
+        const result = await pool.query('DELETE FROM film WHERE film_id = $1', [id]);
+        res.json(result.rows[0]);
+    } catch (err) {
         console.error(err.message);
-      }
+    }
 }
 
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var bodyParser = require('body-parser')
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
@@ -8,6 +9,10 @@ const movie = require("./routes/Movie")
 
 app.use(cors());
 app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.use("/movies", movie)
 
 
